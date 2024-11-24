b57b0a2d864f945057e8c34cf1df6d026033bd0b
shawazi <shawazisonfire@gmail.com>
2024-11-24 10:35:05 -0500

    url adjust

--- daemon-automation/src/App.tsx
+++ daemon-automation/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Router basename={baseUrl}>
+      <Router>
         <Box sx={{ 
           display: 'flex', 
           flexDirection: 'column', 
--- daemon-automation/vite.config.js
+++ daemon-automation/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/automation/' : '/',
+  base: '/',  // Using root path since we have a custom domain
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
