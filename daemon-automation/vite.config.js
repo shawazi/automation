57d60e19ea0369c1b4df9d8218da0fa339c2b1c0
shawazi <shawazisonfire@gmail.com>
2024-11-24 10:14:46 -0500

    adjust base url

---
+++ daemon-automation/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  plugins: [react()],
+  base: '/automation/',  // Update this to match your repository name
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+}))
